95c8d543e8c5a3a73dd08265de8d8fb0806865fb
Max Anurin <maxim.anurin@zxteam.org>
2020-10-07 02:15:14 +0300

    Handle protoc exit code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,28 @@ if [ "${VERBOSE}" == "yes" ]; then
 	set -ex
 fi
 
+PROCOC_EXIT_CODE=255
+
 case "$1" in
 	--target=csharp)
 		echo "Generating C# bindings..."
 		protoc --csharp_out=/data/out --proto_path=/data/in /data/in/*.proto
-		echo "Generation completed successfully..."
-		exit 0
+		PROCOC_EXIT_CODE=$?
 		;;
 	--target=typescript)
 		echo "Generating TypeScript bindings..."
 		protoc --plugin=protoc-gen-ts=/usr/bin/protoc-gen-ts --js_out=import_style=commonjs,binary:/data/out --ts_out=/data/out --proto_path=/data/in /data/in/*.proto
-		echo "Generation completed successfully..."
-		exit 0
+		PROCOC_EXIT_CODE=$?
 		;;
 	*)
 		echo "ERROR. An --target= argument was not passed. Exiting..." >&2
 		exit 255
 		;;
 esac
+
+if [ ${PROCOC_EXIT_CODE} -eq 0 ]; then
+	echo "Generation completed successfully."
+else
+	echo "ERROR. Generation failure with exit code: ${PROCOC_EXIT_CODE}"
+fi
+exit ${PROCOC_EXIT_CODE}

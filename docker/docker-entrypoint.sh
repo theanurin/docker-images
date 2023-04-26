a763c0688f2d582a62a426776b7f75f02ddd473a
Max Anurin <theanurin@gmail.com>
2023-04-26 09:06:08 +0300

    Use "bundle install" instead "bundle update". Allows to execute own command line via image args.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,20 +10,22 @@ if [ $# -eq 0 ]; then
     echo "Warning! Command line arguments is empty."
     echo
     echo "I will execute:"
-    echo "  update bundle"
+    echo "  bundle install"
     echo "  jekyll serve"
     echo
-    echo "Feel free to pass your own command line arguments for Jekyll."
+    echo "Feel free to pass your own command line (as image arguments). Some examples:"
+    echo "  jekyll new .  - Generate a new site"
+    echo "  bundle update - Update your Gemfile.lock"
 
     echo
-    echo "Updating bundle ..."
+    echo "Install bundles ..."
     echo
-    bundle update
+    bundle install
 
     echo
     echo "Starting 'jekyll serve' to serve a site and rebuilds it on changes ..."
     echo
     exec jekyll serve --host 0.0.0.0 --port 4000
 else
-    exec jekyll "$@"
+    exec "$@"
 fi
